86893af679ca96d8d8666a53373f76803137217c
venkatesh-evr <venky1evr@gmail.com>
2025-02-17 20:29:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def transform_text(text):
     return " ".join(y)
 
 
-tk = pickle.load(open("vectorizer.pkl", 'rb'))
+tk = pickle.load(open("vectorized.pkl", 'rb'))
 model = pickle.load(open("model.pkl", 'rb'))
 
 st.title("SMS Spam Detection Model")
