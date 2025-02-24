2c5800bec450010033f716f2d4317dfb3e5169e6
venkatesh-evr <venky1evr@gmail.com>
2025-02-24 13:20:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import nltk
+import nltk 
 nltk.download('punkt')
 nltk.download('punkt_tab')
 nltk.download('stopwords')
@@ -10,7 +10,6 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
-
 
 def transform_text(text):
     text = text.lower()
@@ -36,22 +35,22 @@ def transform_text(text):
 
     return " ".join(y)
 
-
-tk = pickle.load(open("vectorizer.pkl", 'rb'))
-model = pickle.load(open("model.pkl", 'rb'))
+try:
+    vectorizer = pickle.load(open("C:/Users/ASUS/sms-spam-detection-project/vectorizer.pkl", 'rb'))
+    model = pickle.load(open("C:/Users/ASUS/sms-spam-detection-project/model.pkl", 'rb'))
+except Exception as e:
+    st.error(f"Error loading model or vectorizer: {e}")
 
 st.title("SMS Spam Detection Model")
 st.write("Developed by Venkatesh.evr")
-    
 
 input_sms = st.text_input("Enter the SMS")
 
 if st.button('Predict'):
-
     # 1. preprocess
     transformed_sms = transform_text(input_sms)
     # 2. vectorize
-    vector_input = tk.transform([transformed_sms])
+    vector_input = vectorizer.transform([transformed_sms])
     # 3. predict
     result = model.predict(vector_input)[0]
     # 4. Display
