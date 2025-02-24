8e3c8bda34d9fee67d8116a03180f638d25abf4b
venkatesh-evr <venky1evr@gmail.com>
2025-02-24 13:10:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ tk = pickle.load(open("vectorizer.pkl", 'rb'))
 model = pickle.load(open("model.pkl", 'rb'))
 
 st.title("SMS Spam Detection Model")
-st.write("Developed by Dev.loper")
+st.write("Developed by Venkatesh.evr")
     
 
 input_sms = st.text_input("Enter the SMS")
